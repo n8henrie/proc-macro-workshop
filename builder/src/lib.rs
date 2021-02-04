fbf34f825adb35fb678564ee14525dd65d90ff80
Nathan Henrie <nate@n8henrie.com>
2021-02-04 13:00:02 -0700

    Passing 1-3

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,48 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let _input = parse_macro_input!(input as DeriveInput);
 
-    TokenStream::new()
+    let expanded = quote! {
+        impl Command {
+            pub fn builder() -> CommandBuilder {
+                CommandBuilder {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        }
+
+     pub struct CommandBuilder {
+         executable: Option<String>,
+         args: Option<Vec<String>>,
+         env: Option<Vec<String>>,
+         current_dir: Option<String>,
+     }
+
+     impl CommandBuilder {
+         fn executable(&mut self, executable: String) -> &mut Self {
+             self.executable = Some(executable);
+             self
+         }
+         fn args(&mut self, args: Vec<String>) -> &mut Self {
+             self.args = Some(args);
+             self
+         }
+         fn env(&mut self, env: Vec<String>) -> &mut Self {
+             self.env = Some(env);
+             self
+         }
+         fn current_dir(&mut self, current_dir: String) -> &mut Self {
+             self.current_dir = Some(current_dir);
+             self
+         }
+     }
+    };
+    TokenStream::from(expanded)
 }
