d59e91a6cca468bde2df74f18283ae0cddb35070
Nathan Henrie <nate@n8henrie.com>
2021-02-06 02:52:04 -0700

    4 passing

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -41,7 +41,15 @@ pub fn derive(input: TokenStream) -> TokenStream {
          fn current_dir(&mut self, current_dir: String) -> &mut Self {
              self.current_dir = Some(current_dir);
              self
-         }
+        }
+        pub fn build(&mut self) -> Result<Command, Box<dyn std::error::Error>> {
+            Ok(Command{
+                executable: self.executable.take().ok_or("executable is unset")?,
+                args: self.args.take().ok_or("args is unset")?,
+                env: self.env.take().ok_or("env is unset")?,
+                current_dir: self.current_dir.take().ok_or("current_dir is unset")?,
+            })
+        }
      }
     };
     TokenStream::from(expanded)
