8f20da21de74e78eb00eabe501259423c074fee5
Nathan Henrie <nate@n8henrie.com>
2021-03-11 16:41:10 -0700

    Major refactor, 06-optional-field.rs now passing!

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,56 +1,117 @@
-use proc_macro::TokenStream;
+use proc_macro2::{Ident, Span};
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{
+    parse_macro_input, AngleBracketedGenericArguments, Data, DataStruct, DeriveInput,
+    GenericArgument, Path, PathArguments, PathSegment, Type, TypePath,
+};
+
+enum FieldType<'a> {
+    Optional(&'a Type),
+    Required(&'a Type),
+}
+struct BuilderField<'a> {
+    name: &'a Option<Ident>,
+    fieldtype: FieldType<'a>,
+}
 
 #[proc_macro_derive(Builder)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let _input = parse_macro_input!(input as DeriveInput);
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
 
-    let expanded = quote! {
-        impl Command {
-            pub fn builder() -> CommandBuilder {
-                CommandBuilder {
-                    executable: None,
-                    args: None,
-                    env: None,
-                    current_dir: None,
+    let structname = input.ident;
+    let builder_name = Ident::new(&format!("{}Builder", structname), Span::call_site());
+
+    let fields = match input.data {
+        Data::Struct(DataStruct {
+            fields: fieldsnamed,
+            ..
+        }) => fieldsnamed,
+        _ => panic!("No data struct"),
+    };
+
+    let struct_fields = fields
+        .iter()
+        .map(|field| {
+            let name = &field.ident;
+            let fieldtype = match &field.ty {
+                Type::Path(TypePath {
+                    path: Path { segments, .. },
+                    ..
+                }) => match &segments[0] {
+                    PathSegment {
+                        ident,
+                        arguments:
+                            PathArguments::AngleBracketed(AngleBracketedGenericArguments {
+                                args, ..
+                            }),
+                    } if ident == "Option" => match &args[0] {
+                        GenericArgument::Type(t) => Some(FieldType::Optional(t)),
+                        _ => None,
+                    },
+                    _ => None,
+                },
+                _ => None,
+            }
+            .unwrap_or(FieldType::Required(&field.ty));
+            BuilderField { name, fieldtype }
+        })
+        .collect::<Vec<_>>();
+
+    let builder_fields = struct_fields
+        .iter()
+        .map(|BuilderField { name, fieldtype }| {
+            let ty = match *fieldtype {
+                FieldType::Optional(ty) => ty,
+                FieldType::Required(ty) => ty,
+            };
+            quote! { #name: ::std::option::Option::<#ty> }
+        });
+
+    let methods = struct_fields
+        .iter()
+        .map(|BuilderField { name, fieldtype }| {
+            let ty = match *fieldtype {
+                FieldType::Optional(ty) => ty,
+                FieldType::Required(ty) => ty,
+            };
+            quote! {
+                fn #name(&mut self, #name: #ty) -> &mut Self {
+                    self.#name = Some(#name);
+                    self
+                }
+            }
+        });
+
+    let output_fields =
+        struct_fields
+            .iter()
+            .map(|BuilderField { name, fieldtype }| match *fieldtype {
+                FieldType::Optional(_) => quote! { #name: self.#name.take() },
+                FieldType::Required(_) => quote! { #name: self.#name.take().ok_or_else(|| format!("required value is unset: {}", stringify!(#name)))? },
+            });
+
+    (quote! {
+        impl #structname {
+            pub fn builder() -> #builder_name {
+                #builder_name {
+                    ..Default::default()
                 }
             }
         }
 
-     pub struct CommandBuilder {
-         executable: Option<String>,
-         args: Option<Vec<String>>,
-         env: Option<Vec<String>>,
-         current_dir: Option<String>,
+    #[derive(Default)]
+    pub struct #builder_name {
+         #(#builder_fields),*
      }
 
-     impl CommandBuilder {
-         fn executable(&mut self, executable: String) -> &mut Self {
-             self.executable = Some(executable);
-             self
-         }
-         fn args(&mut self, args: Vec<String>) -> &mut Self {
-             self.args = Some(args);
-             self
-         }
-         fn env(&mut self, env: Vec<String>) -> &mut Self {
-             self.env = Some(env);
-             self
-         }
-         fn current_dir(&mut self, current_dir: String) -> &mut Self {
-             self.current_dir = Some(current_dir);
-             self
-        }
-        pub fn build(&mut self) -> Result<Command, Box<dyn std::error::Error>> {
-            Ok(Command{
-                executable: self.executable.take().ok_or("executable is unset")?,
-                args: self.args.take().ok_or("args is unset")?,
-                env: self.env.take().ok_or("env is unset")?,
-                current_dir: self.current_dir.take().ok_or("current_dir is unset")?,
+     impl #builder_name {
+         #(#methods)*
+        pub fn build(&mut self) -> Result<#structname, Box<dyn std::error::Error>> {
+            Ok(#structname{
+                #(#output_fields),*
             })
         }
      }
-    };
-    TokenStream::from(expanded)
+    })
+    .into()
 }
