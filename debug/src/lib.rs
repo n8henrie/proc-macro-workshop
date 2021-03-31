17d103dec2dd9dc32aff0de5b89e1bf55179df29
Nathan Henrie <nate@n8henrie.com>
2021-03-30 23:17:28 -0600

    Pass 02-impl-debug.rs

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,9 +1,39 @@
 use proc_macro::TokenStream;
-use syn::{DeriveInput, parse_macro_input};
-
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(CustomDebug)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = parse_macro_input!(input as DeriveInput);
-    TokenStream::new()
+    let parsed_input = parse_macro_input!(input as DeriveInput);
+
+    let structname = parsed_input.ident;
+    let fields = match parsed_input.data {
+        syn::Data::Struct(
+            syn::DataStruct {
+                fields: syn::Fields::Named(syn::FieldsNamed { named, .. }),
+                ..
+            },
+            ..,
+        ) => named,
+        _ => panic!("no fields!"),
+    };
+
+    let output_fields = fields
+        .iter()
+        .map(|field| {
+            let ident = &field.ident;
+            quote! { .field(stringify!(#ident), &self.#ident) }
+        })
+        .collect::<Vec<_>>();
+
+    (quote! {
+        impl ::std::fmt::Debug for #structname {
+            fn fmt(&self, fmt: &mut ::std::fmt::Formatter) -> ::std::fmt::Result  {
+                fmt.debug_struct(stringify!(#structname))
+                    #(#output_fields)*
+                    .finish()
+            }
+        }
+    })
+    .into()
 }
