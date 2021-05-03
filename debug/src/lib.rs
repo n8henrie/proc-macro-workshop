38f2b1f0aaf5e80f43fd89a514c660d7a4553225
Nathan Henrie <nate@n8henrie.com>
2021-05-03 10:09:39 -0600

    "Passing 04-type-parameters.rs"

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,6 +1,5 @@
 use proc_macro::TokenStream;
 use quote::{quote, ToTokens};
-use syn::{parse_macro_input, DeriveInput};
 
 struct DebugField<T: ToTokens> {
     named: T,
@@ -9,9 +8,13 @@ struct DebugField<T: ToTokens> {
 
 #[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let parsed_input = parse_macro_input!(input as DeriveInput);
+    let parsed_input = syn::parse_macro_input!(input as syn::DeriveInput);
 
     let structname = parsed_input.ident;
+    let generics = add_trait_bounds(parsed_input.generics);
+
+    let (impl_generics, type_generics, where_clauses) = generics.split_for_impl();
+
     let named_fields = match parsed_input.data {
         syn::Data::Struct(
             syn::DataStruct {
@@ -47,6 +50,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
         _ => panic!("no fields!"),
     };
 
+
     let output_fields = named_fields
         .into_iter()
         .map(|DebugField { named, format }| {
@@ -59,14 +63,28 @@ pub fn derive(input: TokenStream) -> TokenStream {
         })
         .collect::<Vec<_>>();
 
-    (quote! {
-        impl ::std::fmt::Debug for #structname {
+
+    let output = quote! {
+        impl #impl_generics ::std::fmt::Debug for #structname #type_generics #where_clauses
+        {
             fn fmt(&self, fmt: &mut ::std::fmt::Formatter) -> ::std::fmt::Result  {
                 fmt.debug_struct(stringify!(#structname))
                     #(#output_fields)*
                     .finish()
             }
         }
-    })
-    .into()
+    };
+    output.into()
+}
+
+// Add a bound `T: HeapSize` to every type parameter T.
+fn add_trait_bounds(mut generics: syn::Generics) -> syn::Generics {
+    for param in &mut generics.params {
+        if let syn::GenericParam::Type(ref mut type_param) = *param {
+            type_param
+                .bounds
+                .push(syn::parse_quote!(::std::fmt::Debug));
+        }
+    }
+    generics
 }
